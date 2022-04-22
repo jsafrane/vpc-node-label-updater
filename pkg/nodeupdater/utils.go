29e3ffc716c017a861a9cc2449a3a821b0634884
Prankul Mahajan <84079249+prankulmahajan@users.noreply.github.com>
2022-04-22 20:10:29 +0530

    rename

--- pkg/nodeupdater/utils.go
+++ pkg/nodeupdater/utils.go
@@ -38,7 +38,7 @@ import (
 )
 
 const (
-	workerIDLabelKey       = "ibm-cloud.kubernetes.io/vpc-id"
+	workerIDLabelKey       = "ibm-cloud.kubernetes.io/vpc-instance-id"
 	failureRegionLabelKey  = "failure-domain.beta.kubernetes.io/region"
 	failureZoneLabelKey    = "failure-domain.beta.kubernetes.io/zone"
 	topologyRegionLabelKey = "topology.kubernetes.io/region"
